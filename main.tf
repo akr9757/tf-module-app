167012a4861b6043d32c63006255ca9a4428a2e3
akr9757 <anilkumareddy9757@gmail.com>
2023-10-15 11:51:21 +0530

    terraform

--- main.tf
+++ main.tf
@@ -46,6 +46,7 @@ resource "aws_autoscaling_group" "asg" {
   max_size            = var.max_size
   min_size            = var.min_size
   vpc_zone_identifier = var.subnet_ids
+  target_group_arns = [ aws_lb_target_group.main.arn ]
 
 
   launch_template {
@@ -61,5 +62,12 @@ resource "aws_autoscaling_group" "asg" {
       value               = tag.value
     }
   }
+}
 
+resource "aws_lb_target_group" "main" {
+  name     = "${var.name}-${var.env}-tg"
+  port     = var.app_port
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+  tags = merge(var.tags, { Name = "${var.name}-${var.env}-tg" })
 }
