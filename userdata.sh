cdb20d367ad57487c8dcdf84b4d329d5bd77cea9
akr9757 <anilkumareddy9757@gmail.com>
2023-10-16 14:15:30 +0530

    terraform

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 labauto ansible
-ansible-pull -i localhost, -U https://github.com/akr9757/roboshop-ansible roboshop.yml -e role_name=${name} -e env=${env}
+ansible-pull -i localhost, -U https://github.com/akr9757/roboshop-ansible roboshop.yml -e role_name=${name} -e env=${env} &>>/opt/ansible.log
